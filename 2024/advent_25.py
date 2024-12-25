9cf8f764ee7320d3efd9eeff44dea810d62ab5cc
Laurence Jupp <laurence@narya.org>
2024-12-25 09:26:34 +0000

    Advent of Code 2024 Day 25
    More tidying / fiddling
    
    Signed-off-by: Laurence Jupp <laurence@narya.org>

--- 2024/advent_25.py
+++ 2024/advent_25.py
@@ -6,13 +6,13 @@ INPUT = 'input_25.txt'
 def get_input():
     ''' Return items as bitmasks '''
     with open(INPUT, 'r', encoding='utf-8') as input25:
-        while item := input25.read(43).split():
-            yield sum((1 << y) << (x * 7) for y in range(7) for x in range(5) if item[y][x] == '#')
+        while item := input25.read(43).replace('\n',''):
+            yield sum(1 << i for i, x in enumerate(item) if x == '#')
 
 def main():
     ''' Check for clashing bits '''
     all_items = set(get_input())
-    total = sum(1 for x in all_items for y in all_items if not x & y)
-    print(f'Part 1: {total//2}')
+    double_count = sum(1 for x in all_items for y in all_items if not x & y)
+    print(f'Part 1: {double_count//2}')
 
 main()
