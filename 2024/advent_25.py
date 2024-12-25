cc66e8bcc7a4488f0d056172cbebaeddf1e07275
Laurence Jupp <laurence@narya.org>
2024-12-25 09:15:25 +0000

    Advent of Code 2024 Day 25
    Just compress it a bit...
    
    Signed-off-by: Laurence Jupp <laurence@narya.org>

--- 2024/advent_25.py
+++ 2024/advent_25.py
@@ -7,22 +7,12 @@ def get_input():
     ''' Return items as hex numbers '''
     with open(INPUT, 'r', encoding='utf-8') as input25:
         while item := input25.read(43).split():
-            value = 0
-            for y in range(7):
-                for x in range(5):
-                    if item[y][x] == '#':
-                        value += (1 << y) << (x * 7)
-            yield value
-
+            yield sum((1 << y) << (x * 7) for y in range(7) for x in range(5) if item[y][x] == '#')
 
 def main():
-    ''' Read in locks and keys then add and check sums for digit overflows '''
+    ''' Check for clashing bits '''
     all_items = set(get_input())
-    total = 0
-    for item in all_items:
-        for other in all_items:
-            if not item & other:
-                total += 1
+    total = sum(1 for x in all_items for y in all_items if not x & y)
     print(f'Part 1: {total//2}')
 
 main()
